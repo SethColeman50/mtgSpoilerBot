beffd90cdeb8cf53e3800e747ce8edb390d1f290
Seth Coleman <84861800+SethColeman50@users.noreply.github.com>
2025-03-24 10:46:23 -0400

    reorganized into src folder to allow python packages to work

---
+++ src/webScrap/scrapping.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+from webScrap.card import Card
+
+header = {
+    "User-Agent": "MTG Spoiler Discord Bot for private use",
+    "contact": "sethcoleman2003@gmail.com"
+}
+
+def scrap():
+    # contents = requests.get('https://www.magicspoiler.com/mtg-set/tarkir-dragonstorm/', headers=header).text
+    contents = open('testingContent.html', 'r').read()
+    soup = BeautifulSoup(contents, 'html.parser')
+
+    cards = soup.find_all("article")[:20] # I'll hopefully never be more than 20 behind
+    output = []
+    for card in cards:
+        time.sleep(1)
+
+        name = card.find("h4").find("a").text
+        image_link = card.find("a").find("img").get("src")
+
+        link = card.find("a").get('href')
+        # card_page = requests.get(link, headers=header).text
+        card_page = open("cardPageWithDescription.html", "r").read()
+        card_page = BeautifulSoup(card_page, 'html.parser')
+        oracle_text = card_page.find("div", attrs={"class": "c-content"}).text
+
+        output.append(Card(
+            name=name,
+            image_link=image_link,
+            oracle_text=oracle_text
+        ))
+    
+    return output
