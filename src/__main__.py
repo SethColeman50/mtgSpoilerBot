cdb9ee73bb0a82fb1ea1b3506178ee6f1dab9ce8
Seth Coleman <84861800+SethColeman50@users.noreply.github.com>
2025-05-29 15:35:27 -0400

    add half of logging

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,20 @@
-from bot.bot import start_bot
+import logging
+import sys
 
 def main():
+    from bot.bot import start_bot
     start_bot()
+
+def get_logger(name: str, filename: str) -> logging.Logger:
+    logger = logging.getLogger(name)
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(handler)
+
+    logging.basicConfig(filename=filename, level=logging.INFO)
+
+    return logger
 
 if __name__ == "__main__":
     main()
--- src/bot/bot.py
+++ src/bot/bot.py
@@ -8,11 +8,15 @@ from discord.ext import tasks
 import traceback
 from database.populate_db import populate_db
 import logging
+import sys
+from src.__main__ import get_logger
 
 MY_USER_ID = 453325432658460685
 
 load_dotenv("../../.env")
 db = Database()
+
+logger = get_logger(__name__, "bot.log")
 
 def start_bot():
     intents = discord.Intents.default()
@@ -23,9 +27,9 @@ def start_bot():
 
     @client.event
     async def on_ready():
-        print(f'We have logged in as {client.user}')
+        logger.info(f'We have logged in as {client.user}')
         if os.getenv("TESTING") is not None:
-            print("Running in dev mode", flush=True)
+            logger.info("Running in dev mode")
 
         populate_db()
         send_new_cards.start()
@@ -34,24 +38,36 @@ def start_bot():
     async def send_new_cards():
         try:
             new_cards = get_new_cards()
-            print("New cards gotten", flush=True)
             if not new_cards:
-                print("No new cards to post", flush=True)
+                logger.info("No new cards to post")
         except Exception:
-            await client.get_user(MY_USER_ID).send(f"An exception happened :(\n```\n{traceback.format_exc()}\n```")
-            print(traceback.format_exc(), flush=True)
+            exception_string = f"An exception happened :(\n```\n{traceback.format_exc()}\n```"
+            await client.get_user(MY_USER_ID).send(exception_string)
+            logger.error(exception_string)
             return
 
+        channels = db.get_all_channels()
+        if channels == []:
+            logger.warning("No channels configured to send messages")
+        
         for card in new_cards:
-            print(card.name, flush=True)
             message = card.oracle_text if '\n' in card.oracle_text or "SRC" not in card.oracle_text else ""
             image = discord.Embed(type="image", description=card.name).set_image(url=card.image_link)
 
-            for channel_id in db.get_all_channels():
+            if not message and not image:
+                logger.info(f"Skipping card {card.name} because message and embed are empty")
+
+            for channel_id in channels:
                 channel = client.get_channel(channel_id)
-                
-                await channel.send(message, embed=image, silent=True)
-                print(f"sent message for {card.name}", flush=True)
+
+                if not channel:
+                    logger.warning(f"Channel {channel_id} not found.")
+                else:
+                    try:
+                        await channel.send(message, embed=image, silent=True)
+                        logger.info(f"Sent message for {card.name} to channel {channel_id}")
+                    except Exception as e:
+                        logger.error(f"Failed to send {card.name} to {channel_id}: {e}")
 
     @client.event
     async def on_message(message):
@@ -63,11 +79,9 @@ def start_bot():
         db.insert_channel(guild_id, channel_id)
 
         await message.channel.send(f"Set channel as <#{channel_id}>")
-        print(f"Set channel in {message.guild.name} to {message.channel_mentions[0]}", flush=True)
+        logger.info(f"Set channel in {message.guild.name} to {message.channel_mentions[0]}")
     
     client.run(os.getenv("TOKEN"))
-
-
 
 def get_new_cards():
     sets = scrap_for_sets()
